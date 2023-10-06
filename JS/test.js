fd232644323dc5cc8add2235b4142250d88c9b36
buidinhhuyhoang20701 <bdhhoang20701@gmail.com>
2023-10-07 00:43:06 +0700

    push

---
+++ JS/test.js
@@ -0,0 +1,17 @@
+
+const menuHeader = document.querySelector('.js-header__body-menu-icon')
+
+const modal = document.querySelector('.js-overlay')
+
+const closeIcon = document.querySelector('.js-close-icon')
+
+function openModal(){
+    modal.classList.add('overlay-open');
+}
+
+function closeModal(){
+    modal.classList.remove('overlay-open');
+}
+
+menuHeader.addEventListener('click', openModal)
+closeIcon.addEventListener('click', closeModal)
